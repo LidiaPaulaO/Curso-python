c932d3cdb0366e2b26191b1eeeff83e404d9567d
Lidia <lidia5000@hotmail.com.br>
2025-11-23 15:57:45 -0300

    Add Bebida and Prato classes for menu item management; refactor app.py for improved structure

--- Estudo POO/POO-SABOR-EXPRESS/app.py
+++ Estudo POO/POO-SABOR-EXPRESS/app.py
@@ -1,26 +1,19 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+# Criação de instâncias de restaurantes
+restaurante1 = Restaurante("Sabor Caseiro", "Comida Caseira")
+restaurante2 = Restaurante("Sushi Express", "Comida Japonesa")
+restaurante3 = Restaurante("Tacos Lokos", "Comida Mexicana")
+
+bebida_suco = Bebida("Suco Melancia",5.0, "grande")
+prato_pao = Prato("Paozinho",2.0,"O melhor paozinho")
 
 def main():
-    """Função principal do programa."""
-
-    # Criação de instâncias de restaurantes
-    restaurante1 = Restaurante("Sabor Caseiro", "Comida Caseira")
-    restaurante2 = Restaurante("Sushi Express", "Comida Japonesa")
-    restaurante3 = Restaurante("Tacos Lokos", "Comida Mexicana")
-
-    # Alterna o status do primeiro restaurante
-    restaurante1.alternar_status()
-
-    # Adiciona avaliações
-    # restaurante1.adicionar_avaliacao("Lidia", 10)
-    # restaurante1.adicionar_avaliacao("Paula", 8)
-    # restaurante2.adicionar_avaliacao("Carlos", 7)
-    # restaurante3.adicionar_avaliacao("Fernanda", 9)
-
-    # Lista todos os restaurantes
-    Restaurante.listar_restaurantes()
 
 
+pass
 # Ponto de entrada do programa
 if __name__ == "__main__":
     main()
---
+++ Estudo POO/POO-SABOR-EXPRESS/modelos/cardapio/prato.py
@@ -0,0 +1,8 @@
+from modelos.cardapio.item_cardapio import ItemCardapio
+class Prato(ItemCardapio):
+    def __init__(self, nome: str, preco: int, descricao: str ):
+        super().__init__(nome,preco)
+        self._descricao = descricao
+    
+    def __str__(self):
+        return self._nome
